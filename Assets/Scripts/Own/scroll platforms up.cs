6effe4818680d9f280f528bb3753dcf1e26cd586
ZaydanShakil <s4402605@glos.ac.uk>
2025-05-21 17:11:02 +0100

    update
    death no work
    why death no work?

--- Assets/Scripts/Own/scroll platforms up.cs
+++ Assets/Scripts/Own/scroll platforms up.cs
@@ -5,12 +5,14 @@ public class PlatformScroller : MonoBehaviour
 
     [SerializeField] private string[] Tags;
     public float scrollSpeed = 1;
+    public float move = 1;
 
     void Update()
     {
+        move = scrollSpeed;
         scrollSpeed = scrollSpeed + Time.deltaTime ;
         // Move the platform upwards over time
-        transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+        transform.position += Vector3.left * move * Time.deltaTime;
 	}
 }
 //change under 50 when goes over 50 
