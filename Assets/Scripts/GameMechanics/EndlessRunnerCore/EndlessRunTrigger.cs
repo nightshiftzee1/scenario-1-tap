4c73d1d2f8c34bb8e50402e5fbf22c5ce4e4a6d7
ZaydanShakil <s4402605@glos.ac.uk>
2025-05-24 17:47:40 +0100

    pipes move and spawn
    pipes need to be adjusted by height and width

--- Assets/Scripts/GameMechanics/EndlessRunnerCore/EndlessRunTrigger.cs
+++ Assets/Scripts/GameMechanics/EndlessRunnerCore/EndlessRunTrigger.cs
@@ -17,7 +17,7 @@ public class EndlessRunTrigger : MonoBehaviour
 
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
     {
         if (Tags.Contains(other.tag) || Tags.Length == 0)
         {
